a1cdc3cd691bd0e5bbc6add3ac51bf8aa3f72b2e
Violetta Sorokina <violetochka@hotmail.com>
2022-12-18 12:43:34 +0300

    7

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+Console.WriteLine($"последняя цифра в {number} равна {number %10}");
+
+else
+{
+    Console.WriteLine("это не трехзначное число");
+}  
