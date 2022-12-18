82835cc3fd2c167bf299fd73a4ec4eb2d76d41e4
Violetta Sorokina <violetochka@hotmail.com>
2022-12-18 14:01:44 +0300

    4

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,20 +1,25 @@
-﻿// Задача 2: Напишите программу, которая на вход
-// принимает два числа и выдаёт, какое число большее, а
-// какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+
 Console.WriteLine("Введите целое число: ");
-int numbera = Convert.ToInt32(Console.ReadLine());
-int numberb = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 int max = 0;
-if (numbera > numberb)
+if (a > b && a > c)
 {
-    max = numbera;
-    Console.WriteLine($"максимальное число{numbera}");
+    max = a;
+    Console.WriteLine($"максимальное число {a}");
+}
+else if (b > c)
+{
+    max = b;
+    Console.WriteLine($"максимальное число {b}");
 }
 else
-    Console.WriteLine($"максимальное число {numberb}");
-{
-
-}
+    max = c;
+Console.WriteLine($"максимальное число {c}");
