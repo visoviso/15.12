1827c1b5e1fcb8e5ed66e5da6f08805bff515395
Violetta Sorokina <violetochka@hotmail.com>
2022-12-19 20:52:21 +0300

    1

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -10,5 +10,22 @@ int secondDigit = number % 10;
 
 
 
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.Write($"Наибольшая цифра числа => { result}");
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.Write($"Наибольшая цифра числа => {result}");
+
+int maxDigit = MaxDigit(number);
+Console.Write($"Наибольшая цифра числа => {maxDigit}");
+
+int maxDigit1 = MaxDigit(45);
+Console.Write($"Наибольшая цифра числа => {maxDigit1}");
+
+int maxDigit2 = MaxDigit(78);
+Console.Write($"Наибольшая цифра числа => {maxDigit1}");
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return result;
+}
