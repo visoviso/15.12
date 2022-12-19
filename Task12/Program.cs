2ceb014d389df7840bc5f13650e85fa42440d25f
Violetta Sorokina <violetochka@hotmail.com>
2022-12-19 21:45:07 +0300

    1

---
+++ Task12/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+Console.WriteLine("Введите два числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a % b;
+
+if (result == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("остаток от деления" + result);
+}
+// int number = new Random().Next(100, 100);
+// Console.WriteLine($"случайное число из отрезка 100-999 => {number}");
+
+// // int firstDigit = number / 100;
+// // int secondDigit = number % 10;
+// // int result = firstDigit * 10 + secondDigit;
+
+// int maxDigit2 = RemovesecondDigit(number);
+// Console.Write($"два числа, которые будут показаны => {maxDigit2}");
+
+// int RemovesecondDigit(int number)
+// {
+//     int firstDigit = number / 100;
+//     int secondDigit = number % 10;
+//     int result = firstDigit * 10 + secondDigit;
+//     return result;
+// }
