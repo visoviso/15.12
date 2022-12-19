a6c5674e7e3191b26760cb13d682bc6169c5c746
Violetta Sorokina <violetochka@hotmail.com>
2022-12-19 20:32:34 +0300

    1

---
+++ Task09/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"случайное число из отрезка 10-99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа => {secondDigit}");
+
+
+
+
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.Write($"Наибольшая цифра числа => { result}");
