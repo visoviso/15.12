24f7155aa400f1f5ee67a6e11be330623f2d9c07
Violetta Sorokina <violetochka@hotmail.com>
2022-12-21 21:58:03 +0300

    1

---
+++ Task14/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(BothMultiple(number) ? "Да" : "Нет");
+
+bool BothMultiple(int num, int num1 = 7, int num2 = 23)
+{
+    return num % num1 == 0 && num % num2 ==0;
+}
+
