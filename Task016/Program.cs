60b524773621aad6d950220775b22b010509b9eb
Violetta Sorokina <violetochka@hotmail.com>
2022-12-21 22:58:27 +0300

    2

--- Task016/Program.cs
+++ Task016/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Введите 2 числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine(SquareNumber(number1, number2) ? "Да" : "Нет");
 
 bool SquareNumber(int num1, int num2)
-
 {
     return num1 * num1 == num2 || num2 * num2 == num1;
 }
