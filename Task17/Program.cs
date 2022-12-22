48824559564fce57ca32ecec03aafbe66979aefa
Violetta Sorokina <violetochka@hotmail.com>
2022-12-22 20:38:27 +0300

    1

---
+++ Task17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите координеты точки ");
+
+Console.Write("X:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0
+                       ? $"Указанные координаты соответствуют четверти -> {quarter}"
+                       : "введены некорректные координаты";
+Console.WriteLine(result);
+
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+}
