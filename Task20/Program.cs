ecd7f5d1ff8d2cafa587dbfb9c453fe3dd0bdaa9
Violetta Sorokina <violetochka@hotmail.com>
2022-12-22 21:28:56 +0300

    1

---
+++ Task20/Program.cs
@@ -0,0 +1,29 @@
+﻿принимает на вход координаты двух точет и находит расстояние между ними в 2d пространстве
+a (3,6) b(2, 1)
+
+Console.WriteLine("введите координеты точки A ");
+
+Console.Write("X:");
+int ax = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y:");
+int ay = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите координеты точки B ");
+
+Console.Write("X:");
+int bx = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y:");
+int by = Convert.ToDouble(Console.ReadLine());
+
+
+int Distance(int x1, int y1, int x2, int y2)
+
+
+
+Math.Pow(2, 2);
+double d = Math.Sqrt(355);
+
+double num = 5.099873456;
+double numRound = Math.Round(num, 2, MidpointRounding.ToZero);
+Console.WriteLine(numRound);
+
+
