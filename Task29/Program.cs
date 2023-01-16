f39ff4f7857b1cdfa4839d4767813b1415d828e0
Violetta Sorokina <violetochka@hotmail.com>
2023-01-16 19:51:20 +0300

    2

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая 
+// 1.задаёт массив из 8 элементов 
+// 2.выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+int[] array = new int[] { 1, 2, 5, 7, 19, 6, 1, 33 };
+Fillarray(array);
+// PrintArray(array);
+
+void Fillarray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+
+}
