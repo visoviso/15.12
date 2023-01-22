d42b6381b3dad96fc075dd69d7e2ee1bd46b8d58
Violetta Sorokina <violetochka@hotmail.com>
2023-01-22 20:59:29 +0300

    1

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -21,14 +21,13 @@ int[] array = new int[8]; // 0,1,2,3,4,5,6,7
 FillArray(array);
 PrintArray(array);
 
-void Fillarray(int[]arr)
+void FillArray(int[] arr)
 {
-    
-    
-    // for (int i = 0; i < arr.Length; i++)
-    // {
-    //     arr[i] = new Random().Next(0, 2);
-    // }
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
 }
 
 void PrintArray(int[] arr)
@@ -38,24 +37,3 @@ void PrintArray(int[] arr)
         Console.Write(arr[i] + " ");
     }
 }
-int[] array = new int[8]; // 0,1,2,3,4,5,6,7
-
-            FillArray(array);
-            PrintArray(array);
-
-            void FillArray(int[] arr)
-            {
-                Random rnd = new Random();
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    arr[i] = rnd.Next(0, 2);
-                }
-            }
-
-            void PrintArray(int[] arr)
-            {
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    Console.Write(arr[i] + " ");
-                }
-            }
