6259d466f55c06ee8d250a395cac0dce970ebd8b
Violetta Sorokina <violetochka@hotmail.com>
2023-01-22 19:20:01 +0300

    1

---
+++ Task44/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+
+
+
+// Console.Writeline ( " введите число: ")
+// nt number = Convert.ToInt32(Console.ReadLine());
+//  int [] array = GetFibonacciArray(number);
+//  PrintArray(array);
+
+// int[] GetFibonacciArray(int size);
+// {
+// int[] result = new int[size];
+// result[1] = 1;
+// for (int i = 0; i < result.Length i++)
+// {
+//     result [i] = result [i-1]+result [i-2];
+//     }
+//     return result;
+// }
+// void PrintArray(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+//         else Console.Write(arr[i]);
+//     }
+//     Console.Write("]");
+// }
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = GetFibonacciArray(number);
+// PrintArray(array);
+
+
+int[] GetFibonacciArray(int size)
+{
+    int[] result = new int[size];
+    result[1] = 1;
+    for (int i = 2; i < result.Length; i++)
+    {
+        result[i] = result[i - 1] + result[i - 2];
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + " ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine();
+}
