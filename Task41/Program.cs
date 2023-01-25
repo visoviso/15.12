f030626669af787674695454c6531b4d40658d2e
Violetta Sorokina <violetochka@hotmail.com>
2023-01-26 00:06:28 +0300

    4

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,115 +1,14 @@
 ﻿// ДЗ
 // Задача 41: 
-// 1.Пользователь вводит с клавиатуры M чисел. 
-// 2.Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 1.Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите числа через запятую: ");
-int[] numbers = Count(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
+Console.Write("Введите элементы через запятую: ");
+int[] input = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+for (int i = 0; i < input.Length; i++)
 {
-    if (numbers[i] > 0)
-    {
-        sum++;
-    }
+    if (input[i] > 0) count++;
 }
-
-Console.WriteLine($"количество значений больше 0 = {sum}");
-
-
-int[] Count(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
-
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
-
-
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
-
-
-
-
-// fffff
-// int[] CreateArrayRndInt(int size, int min, int max)
-// {
-//     int[] arr = new int[size];
-//     Random rnd = new Random();
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = rnd.Next(min, max + 1);
-//     }
-//     return arr;
-// }
-
-// void PrintArray(int[] arr)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         if (i < arr.Length - 1) Console.Write(arr[i] + ",");
-//         else Console.Write(arr[i]);
-//     }
-//     Console.Write("]");
-// }
-// // Array.Reverse(aaray);
-// // [1 2 3 4 5] -> [5 4 3 2 1]
-// void ReverseArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length / 2; i++)
-//     {
-//         int temp = arr[i];
-//         arr[i] = arr[arr.Length - 1 - i];
-//         arr[arr.Length - 1 - i] = temp;
-//     }
-// } 
-
-
-
-// int[] array = CreateArrayRndInt(5, 1, 10);
-// PrintArray(array);
-// ReverseArray(array);
-// PrintArray(array);
-// Array.Reverse(array);
-// PrintArray(array);
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
