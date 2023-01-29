3a8d6dffec48a3710cc9e6c7ece4fc8c0065c31f
Violetta Sorokina <violetochka@hotmail.com>
2023-01-30 00:43:39 +0300

    1

---
+++ Task52/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+// Задача 52. 
+// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int[,] matrix = new int[3, 4];
+double sum =0;
+
+void CreateMatrixRndInt(int[,] array)
+{
+Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(10, 20);
+        }
+    }
+} 
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+CreateMatrixRndInt(matrix);
+PrintMatrix(matrix);
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+  
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{ sum / matrix.GetLength(0)} ");
+}
+
