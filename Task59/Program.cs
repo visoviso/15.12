b576e62d4ead375c80f8b5a29df1b150d24fe46b
Violetta Sorokina <violetochka@hotmail.com>
2023-01-29 18:17:00 +0300

    1

---
+++ Task59/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 2 3
+// 4 2 4
+// 2 6 7
+
+int[,] RemoveRowColumnCrossed(int[,] matrix, int removeRow, int removeColumn)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+    int m = 0;
+    int n = 0;
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        if (i == removeRow) m++;
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            if (j == removeColumn) n++;
+            newMatrix[i,j] = matrix[m,n];
+            n++;
+        }
+        m++;
+        n = 0;
+    }
+    return newMatrix;
+}
+
+
+int[] IndexMinVal(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int indexRow = 0;
+    int indexColumn = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                indexRow = i;
+                indexColumn = j;
+            }
+        }
+    }
+    return new int
+}
