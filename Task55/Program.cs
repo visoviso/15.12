a6b1d172abbda8de7af898bae7b32249af4b251c
Violetta Sorokina <violetochka@hotmail.com>
2023-01-29 18:15:31 +0300

    1

---
+++ Task55/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5},");
+            else Console.Write($"{matrix[i, j], 5}  ");
+        }
+        Console.WriteLine("]");
+    }   
+}
+void ReverseMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) -1 ; j++)
+        {
+            temp = matrix[i, j + 1];
+            matrix[i, j + 1] = matrix[j + 1, i];
+            matrix[j + 1, i] = temp;
+        }
+    }
+
+}
+
+int[,] array2d = CreateMatrixRndInt(7, 7, -20, 20);
+PrintMatrix(array2d);
+if (array2d.GetLength(0) == array2d.GetLength(1))
+{
+    ReverseMatrix(array2d);
+    Console.WriteLine();
+    PrintMatrix(array2d);
+}
+else Console.WriteLine("Введенная матрица не квадратная.");
+void ChangeRowColumnsMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0)-1; i++)
+    {
+        for (int j = i+1; j < matrix.GetLength(1); j++)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+/*int[,] ChangeRowColumnsMatrix(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = matrix[j, i];
+        }
+    }
+    return newMatrix;
+}*/
+
+
