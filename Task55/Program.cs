76efbc73ec915f3b77e240a5b04f70d3a008d015
Violetta Sorokina <violetochka@hotmail.com>
2023-02-05 21:08:22 +0300

    2

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -80,4 +80,3 @@ void ChangeRowColumnsMatrix(int[,] matrix)
     return newMatrix;
 }*/
 
-
---
+++ Task56/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+void MinSumRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int rowSum = 0;
+ 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+    rowSum += array[i, j];
+    array[i, 0] = rowSum;
+        }
+        Console.WriteLine($"{rowSum}");
+    }
+}
+int[,] matrix = CreateMatrixRndInt(3, 4, 10, 20);
+PrintMatrix(matrix);
+Console.WriteLine("сумма элементов в строке: ");
+MinSumRow(matrix);
+
