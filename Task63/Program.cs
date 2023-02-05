b310e62be0e1696c4672c3e35069d7065dc7efdb
Violetta Sorokina <violetochka@hotmail.com>
2023-02-05 23:19:38 +0300

    1

---
+++ Task63/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
