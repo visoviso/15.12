ef0f7910f805965463c012e3f495332d2460e808
Violetta Sorokina <violetochka@hotmail.com>
2023-02-05 23:20:21 +0300

    1

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigit = SumDigit(number);
+Console.WriteLine(sumDigit);
+
+int SumDigit (int n)
+{    
+    if(n == 0) return 0;
+    return n % 10 + SumDigit(n / 10);
+}
+
+
